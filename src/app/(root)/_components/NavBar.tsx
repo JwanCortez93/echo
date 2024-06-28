362ea75edba1be30b27ee27bc025c9f6f4bff44c
JwanCortez93 <jwancortez@gmail.com>
2024-06-28 06:40:47 -0300

    NavBar init

--- src/app/(root)/_components/NavBar.tsx
+++ src/app/(root)/_components/NavBar.tsx
@@ -1,5 +1,47 @@
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
+import { LogOut } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav className="navbar">
+      <Link href="/" className="hidden items-center gap-1 lg:flex">
+        <Image
+          src={"/logo/svg/logo-no-background.svg"}
+          width={200}
+          height={200}
+          alt="Echo Logo"
+        />
+      </Link>
+      <Link href="/" className="lg:hidden flex items-center gap-1">
+        <Image
+          src={"/logo/png/echo-favicon-color.png"}
+          width={50}
+          height={50}
+          alt="Echo Logo"
+        />
+      </Link>
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <LogOut className="text-secondary" />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
